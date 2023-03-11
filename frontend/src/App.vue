fb226c22a26397f19228b36c30797cbd5e420fb9
Mizba-Anjum <89572205+Mizba-Anjum@users.noreply.github.com>
2023-03-10 18:15:27 -0600

    if not logged in go to login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,6 +19,9 @@ export default {
     axios.get(`${apiURL}/org`).then((res) => {
       this.orgName = res.data.name
     })
+    if (!this.user.isLoggedIn) {
+      this.$router.push("/userlogin"); //redirect to login if not logged in
+    }
   }
 }
 </script>
