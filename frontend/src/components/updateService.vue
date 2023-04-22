e646e46194152ad5ce5752ed2ec5b3ea66d9531b
Mizba-Anjum <89572205+Mizba-Anjum@users.noreply.github.com>
2023-04-22 18:13:13 -0500

    deleted event list from update service page

--- frontend/src/components/updateService.vue
+++ frontend/src/components/updateService.vue
@@ -151,62 +151,6 @@ export default {
           </div>
         </div>
 
-        <hr class="mt-10 mb-10" />
-
-        <!-- Service Event Information -->
-        <div
-          class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-        >
-          <h2 class="text-2xl font-bold">Events for Service</h2>
-
-          <div class="flex flex-col col-span-2">
-            <table class="min-w-full shadow-md rounded">
-              <thead class="bg-gray-50 text-xl">
-                <tr>
-                  <th class="p-4 text-left">Event Name</th>
-                  <th class="p-4 text-left">Date</th>
-                </tr>
-              </thead>
-              <tbody class="divide-y divide-gray-300">
-                <!-- allow click through to event details -->
-                <tr
-                  @click="editEvent(event._id)"
-                  v-for="event in eventsRegistered"
-                  :key="event._id"
-                >
-                  <td class="p-2 text-left">{{ event.name }}</td>
-                  <td class="p-2 text-left">
-                    {{ formattedDate(event.date) }}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <div class="flex flex-col">
-            <!-- fixed weird selection duplication bug -->
-            <VueMultiselect
-              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              v-model="eventsSelected"
-              :options="eventsAll"
-              :custom-label="nameWithDate"
-              :multiple="true"
-              :close-on-select="true"
-              placeholder="Select Events to be added"
-              label="date"
-              track-by="name"
-            />
-            <div class="flex justify-between">
-              <button
-                @click="addToEvent"
-                type="submit"
-                class="mt-5 bg-red-700 text-white rounded"
-              >
-                Add Service to Selected Events
-              </button>
-            </div>
-          </div>
-        </div>
       </form>
     </div>
   </main>
