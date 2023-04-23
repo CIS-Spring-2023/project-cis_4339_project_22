159fb7591901f62e5142276ba07b872f5564fbea
Mizba-Anjum <89572205+Mizba-Anjum@users.noreply.github.com>
2023-04-22 20:25:59 -0500

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.use('/clients', require('./routes/clients'))
 app.use('/events', require('./routes/events'))
 app.use('/org', require('./routes/org'))
 // added route for services
-app.use('/services', require('./routes/services'))
+app.use('/services', require('./routes/service'))
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
